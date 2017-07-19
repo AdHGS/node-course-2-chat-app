df3871d4b18f2a9bdd862bf867a40bc94a5a6e9e
Adam Hickson GS <adam.hickson@gameseek.co.uk>
2017-07-19 17:20:27 +0100

    'generateMessageUtil'

--- server/server.js
+++ server/server.js
@@ -1,8 +1,9 @@
 const path = require('path');
 const http = require('http');
 const express = require('express');
-const socketIO = require('socket.io')
+const socketIO = require('socket.io');
 
+const {generateMessage} = require('./utils/message');
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 var app = express();
@@ -14,25 +15,13 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('Client connected');
 
-  socket.emit('newMessage', {
-    from: 'Admin',
-    text: 'Welcome, user!',
-    createdAt: new Date().getTime()
-  })
+  socket.emit('newMessage', generateMessage('Admin','Welcome to the ChannelName, User!'));
 
-  socket.broadcast.emit('newMessage', {
-    from: 'Admin',
-    text: 'User has joined the room!',
-    createdAt: new Date().getTime()
-  })
+  socket.broadcast.emit('newMessage', generateMessage('Admin', 'User joined the channel!'));
 
   socket.on('createMessage', (message) => {
     console.log('createMessage', message);
-    io.emit('newMessage', {
-      from: message.from,
-      text: message.text,
-      createdAt: new Date().getTime()
-    });
+    io.emit('newMessage', generateMessage(message.from, message.text));
     // socket.broadcast.emit('newMessage', {
     //   from: message.from,
     //   text: message.text,
